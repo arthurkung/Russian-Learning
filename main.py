afcb87a058b90a31675d96def05170ba9d972d7e
Arthur Kung <arthurkung1006@gmail.com>
2020-06-23 21:27:41 +0100

    put path var outside functions

--- main.py
+++ main.py
@@ -6,24 +6,25 @@ import zipfile
 import numpy as np
 import pandas as pd
 # import matplotlib
+
+directory_to_extract_to='resource'
+unzipped_filename = ['sentences (1).csv'  ,'verses.csv']
+
 # function for Unzip file
 def unzip_file():
     path_to_zip_file='resource/Russian-English-Bible.zip'
-    directory_to_extract_to='resource'
     with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
         zip_ref.extractall(directory_to_extract_to)
     print('unzip complete')
 
 # function for remove unzipped files
 def remove_unzip_file():
-    unzipped_filename = ['sentences (1).csv'  ,'verses.csv']
     for file in unzipped_filename:
         file_path = directory_to_extract_to + '/' + file
         os.remove(file_path)
         print('This file is removed: {}'.format(file))
 
 # unzip_file()
-# sentences_csv = directory_to_extract_to + '/' + unzipped_filename[0]
-# sentences_df = pd.read_csv(sentences_csv)
+sentences_csv = directory_to_extract_to + '/' + unzipped_filename[0]
+sentences_df = pd.read_csv(sentences_csv)
 
-# print(len)
